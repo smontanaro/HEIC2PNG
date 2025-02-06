a6e9d3d541fca377127f02d9d7b110286e027832
Skip Montanaro <skip.montanaro@gmail.com>
2025-02-06 10:45:02 -0600

    support stdin/stdout in addition to files, loosen some checks on format

--- src/heic2png/heic2png.py
+++ src/heic2png/heic2png.py
@@ -1,13 +1,17 @@
+import sys
 import subprocess
 from pathlib import Path
 from typing import Optional
+
 from PIL import Image
 from pillow_heif import register_heif_opener
 
 register_heif_opener()
 
 class HEIC2PNG:
-    def __init__(self, image_file_path: str, quality: Optional[int] = None, overwrite: bool = False):
+    def __init__(self, image_file_path: Optional[str] = "",
+                 quality: Optional[int] = None,
+                 overwrite: bool = False):
         """
         Initializes the HEIC2PNG converter.
 
@@ -15,38 +19,42 @@ class HEIC2PNG:
         :param quality: Quality of the converted PNG image (1-100).
         :param overwrite: Whether to overwrite the file if it already exists.
         """
-        self.image_file_path: Path = Path(image_file_path)
+        if not image_file_path:
+            self.image_file_path = sys.stdin
+        # TODO: I'm not a Python type hints maven. The image_file_path type
+        # needs to reflect that an open file object is also acceptable.
+        elif hasattr("fileno", image_file_path):
+            pass                        # already open file
+        else:
+            self.image_file_path: Path = Path(image_file_path)
         self.quality: Optional[int] = quality
         self.overwrite: bool = overwrite
 
-        if not self.image_file_path.is_file():
-            raise FileNotFoundError(f"The file {image_file_path} does not exist.")
-
-        if self.image_file_path.suffix.lower() != '.heic':
-            raise ValueError("The provided file is not a HEIC image.")
-
         self.image: Image.Image = Image.open(self.image_file_path)
 
-    def save(self, output_image_file_path: Optional[str] = None, extension: str = '.png') -> Path:
+    def save(self, output_image_file_path: Optional[str] = "",
+             extension: str = '.png') -> Path:
         """
-        Converts and saves the HEIC image to PNG format.
+        Converts and saves the input image to another format.
+
+        The default output format is PNG.
 
         :param output_image_file_path: Path to save the converted PNG image.
         :param extension: The file extension of the converted image.
         :return: Path where the converted image is saved.
         """
-        if output_image_file_path:
-            output_path: Path = Path(output_image_file_path)
-            if output_path.suffix.lower() != extension:
-                raise ValueError("The output file extension does not match the specified extension.")
+        if not output_image_file_path:
+            output_path = sys.stdout
         else:
-            output_path: Path = self.image_file_path.with_suffix(extension)
+            output_path: Path = Path(output_image_file_path)
 
-        if not self.overwrite and output_path.exists():
-            raise FileExistsError(f"The file {output_path} already exists.")
-
+        # TODO: Since this now accepts an open file object, an explicit output
+        # file format should be supported, but I don't know if the file
+        # extension is sufficient to pass as the optional format parameter.
         self.image.save(output_path)
 
+        # TODO: Skip this step if the output format isn't PNG or pngquant can't
+        # be found?
         # Optimize PNG with pngquant if quality is specified
         if self.quality and self.quality != 100:
             quality_str: str = f'{self.quality}-{self.quality}'
